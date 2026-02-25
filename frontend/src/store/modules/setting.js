f75940bc6293e4e5f7c0c3d0945273fcb48ffc25
Fankekeke <1258846421@qq.com>
2026-02-25 09:49:54 +0800

    fix - 分析设备操作相关性

--- frontend/src/store/modules/setting.js
+++ frontend/src/store/modules/setting.js
@@ -12,7 +12,7 @@ export default {
     isMobile: false,
     theme: db.get('THEME', 'light'),
     layout: db.get('LAYOUT', 'side'),
-    systemName: 'MQTT数据解析的设备联动平台',
+    systemName: '设备联动平台',
     copyright: `${new Date().getFullYear()} <a>fank</a>`,
     multipage: getBooleanValue(db.get('MULTIPAGE'), true),
     fixSiderbar: getBooleanValue(db.get('FIX_SIDERBAR'), true),
--- frontend/src/views/login/Login.vue
+++ frontend/src/views/login/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <a-card :bordered="false" hoverable style="margin-top: 30px;background-color: #f9f9f9">
-    <div style="text-align: left;font-size: 16px;margin-bottom: 30px"><b>MQTT数据解析的设备联动平台</b></div>
+    <div style="text-align: left;font-size: 16px;margin-bottom: 30px"><b>设备联动平台</b></div>
     <div class="login">
       <a-form @submit.prevent="doLogin" :autoFormCreate="(form) => this.form = form">
         <div>
